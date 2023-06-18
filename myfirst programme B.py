944fa5b3d4fb1469a1c9d769cfcf14b794d380bf
saabith2009 <136815728+saabith2009@users.noreply.github.com>
2023-06-18 21:21:49 +0530

    Add files via upload

---
+++ myfirst programme B.py
@@ -0,0 +1,7 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\FKSABEER\AppData\Local\Programs\Python\Python311\puthiya payanankal\myfirst programme.py
+HELLO WORLD
+what is your name?SAABITH
+Hello SAABITH
---
+++ myfirst programme.py
@@ -0,0 +1,4 @@
+#KABEER_SAABITH_OFFICIAL
+print('HELLO WORLD')
+person=input('what is your name?')
+print('Hello',person)
